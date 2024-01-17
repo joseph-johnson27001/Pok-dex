2bec8d3bb8d43e163ac5e742b2a8a6f714831a9b
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2024-01-17 10:00:37 +0700

    API call getting back pics

--- src/services/pokemonService.js
+++ src/services/pokemonService.js
@@ -1,12 +1,22 @@
+// src/services/pokemonService.js
 import axios from "axios";
 
 const BASE_URL = "https://pokeapi.co/api/v2/";
 
 export const getPokemonList = async () => {
   try {
-    console.log("RUNNING");
     const response = await axios.get(`${BASE_URL}pokemon?limit=100`);
-    return response.data.results;
+    const pokemonList = response.data.results;
+
+    // Fetch additional details for each Pokemon
+    const detailedPokemonList = await Promise.all(
+      pokemonList.map(async (pokemon) => {
+        const detailsResponse = await axios.get(pokemon.url);
+        return detailsResponse.data;
+      })
+    );
+
+    return detailedPokemonList;
   } catch (error) {
     console.error("Error fetching Pokemon list:", error);
     throw error;
