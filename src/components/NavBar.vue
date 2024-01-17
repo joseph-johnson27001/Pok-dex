018f781797ceeedb3d430f118ef89c939d2782d7
joseph-johnson27001 <joseph.johnson27001@gmail.com>
2024-01-17 20:35:11 +0700

    Navbar updates

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,9 @@
 <template>
   <nav>
-    <img class="pokeball" src="@/assets/pokeball.png" alt="Pokeball" />
-    <div class="logo">Pokedex</div>
+    <div class="navigation-container">
+      <img class="pokeball" src="@/assets/pokeball.png" alt="Pokeball" />
+      <span class="logo">Pokédex</span>
+    </div>
   </nav>
 </template>
 
@@ -13,20 +15,29 @@ export default {};
 nav {
   background-color: #ef5350;
   color: #fff;
+  /* justify-content: center; */
+}
+
+.navigation-container {
   padding: 10px;
+  /* margin-left: -20px; */
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between; /* Add this line */
+  max-width: 1200px;
+  margin: auto;
 }
 
 .logo {
   font-size: 1.5rem;
   font-weight: bold;
   color: #fff;
+  flex-grow: 1; /* Add this line */
 }
 
 .pokeball {
   width: 30px;
-  margin-right: 10px;
+  margin-left: -20px;
+  cursor: pointer;
 }
 </style>
